0089722751c04e043432e7d6e5e5d72e541cba76
Zen <exeat@pronton.me>
2025-08-27 13:49:34 -0400

    The Header for all my code

---
+++ Header.py
@@ -0,0 +1,18 @@
+''' 
+-----------------------------------------------------------------------------
+Solution:       Lab #.#: <name of lab>
+-----------------------------------------------------------------------------
+Developer:      Zen Toppenberg
+Course:         Intro to Programming & Logic – CITC-1301
+Creation Date:  8/27/25)
+Last Mod Date:  8/27/25)
+E-mail Address: nxtoppenberg@senators.ws.edu
+-----------------------------------------------------------------------------
+Purpose - <general description of program's purpose>
+-----------------------------------------------------------------------------
+Description of input:
+<short description of program's inputs>
+Description of output:
+<short description of program's outputs>
+-----------------------------------------------------------------------------
+'''
