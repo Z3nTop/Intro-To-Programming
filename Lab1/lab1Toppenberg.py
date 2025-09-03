79fa5d2702e10995c3c43ebda5cd63b5f00ba9cc
Zen <exeat@pronton.me>
2025-09-03 13:15:10 -0400

    File for Lab1

---
+++ Lab1/lab1Toppenberg.py
@@ -0,0 +1,22 @@
+''' 
+-----------------------------------------------------------------------------
+Solution:       toppenbergLab1
+-----------------------------------------------------------------------------
+Developer:      Zen Toppenberg
+Course:         Intro to Programming & Logic – CITC-1301-H91
+Creation Date:  9/3/25
+Last Mod Date:  9/3/25
+E-mail Address: nxtoppenberg@senators.ws.edu
+-----------------------------------------------------------------------------
+Purpose - Ask the user for indomation about thier character
+-----------------------------------------------------------------------------
+Description of input:
+<short description of program's inputs>
+Description of output:
+<short description of program's outputs>
+-----------------------------------------------------------------------------
+'''
+
+#Askes the user for infoamtuon about thier charater
+
+print("Wellcome to the world of DND")
