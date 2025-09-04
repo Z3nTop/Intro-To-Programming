5c134807a25c2bc47ab75aa388fa7e31908726b3
ZenExeat <you@example.com>
2025-09-04 08:42:31 -0400

    edit 2

--- Lab1/lab1Toppenberg.py
+++ Lab1/lab1Toppenberg.py
@@ -5,29 +5,31 @@ Solution:       toppenbergLab1
 Developer:      Zen Toppenberg
 Course:         Intro to Programming & Logic – CITC-1301-H91
 Creation Date:  9/3/25
-Last Mod Date:  9/3/25
+Last Mod Date:  9/4/25
 E-mail Address: nxtoppenberg@senators.ws.edu
 -----------------------------------------------------------------------------
 Purpose - Ask the user for indomation about thier character
 -----------------------------------------------------------------------------
 Description of input:
-<short description of program's inputs>
+information about the users character
 Description of output:
-<short description of program's outputs>
+Outputs the genen information 
 -----------------------------------------------------------------------------
 '''
 
-#Askes the user for infoamtuon about thier charater
+#Askes the user for infoamtuon about thier character
 
 print("Wellcome to the world of DND")
 name = input("Let's start with your name ")
 level = int(input("What level are you? "))
 hp = int(input("How many hit points do you have? "))
 gold = int(input("How much gold do you have? "))
+rations = gold / 5
 
-#display the charoter info
+#display the character info
 print("Here is your character sheet")
 print("Name:", name)
 print("Level:", level)
-print("Hit points: ", hp)
+print("Hit points:", hp)
 print("Gold: ", gold)
+print("You can use the gold you have to buy "f'{rations:.0f}' " rations")
