0e89af352eb406ec06351851e22cfa1956fb75e2
Zen <exeat@pronton.me>
2025-09-05 22:46:52 -0400

    Fixed spelling

--- Lab1/lab1Toppenberg.py
+++ Lab1/lab1Toppenberg.py
@@ -5,19 +5,19 @@ Solution:       toppenbergLab1
 Developer:      Zen Toppenberg
 Course:         Intro to Programming & Logic – CITC-1301-H91
 Creation Date:  9/3/25
-Last Mod Date:  9/4/25
+Last Mod Date:  9/5/25
 E-mail Address: nxtoppenberg@senators.ws.edu
 -----------------------------------------------------------------------------
-Purpose - Ask the user for indomation about thier character
+Purpose - Ask the user for information about thier character
 -----------------------------------------------------------------------------
 Description of input:
 information about the users character
 Description of output:
-Outputs the genen information 
+Outputs the given information 
 -----------------------------------------------------------------------------
 '''
 
-#Askes the user for infoamtuon about thier character
+#Asks the user for information about thier character
 
 print("Wellcome to the world of DND")
 name = input("Let's start with your name ")
