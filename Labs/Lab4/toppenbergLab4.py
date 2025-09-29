a007319c192d7a4f0dbf672d8e50c28ae8953379
Zen <exeat@pronton.me>
2025-09-29 13:18:41 -0400

    poop

--- Labs/Lab4/toppenbergLab4.py
+++ Labs/Lab4/toppenbergLab4.py
@@ -1,6 +1,6 @@
 ''' 
 -----------------------------------------------------------------------------
-Solution:       toppenbergLab
+Solution:       toppenbergLab4
 -----------------------------------------------------------------------------
 Developer:      Zen Toppenberg
 Course:         Intro to Programming & Logic – CITC-1301-H91
@@ -11,23 +11,25 @@ E-mail Address: nxtoppenberg@senators.ws.edu
 Purpose - <general description of program's purpose>
 -----------------------------------------------------------------------------
 Description of input:
-<short description of program's inputs>
+Number that are not 67 to be added
 Description of output:
-<short description of program's outputs>
+
 -----------------------------------------------------------------------------
 '''
-x = 0
-number = 0
+#sets vars for the program
+num = 0
+tottal = 0
+#Takes the user input and adds it to it's self
 
-while number != 67:
-    number = int(input("Enter a positive number… enter 67 to end the program: "))
-    if number < 0:
-        print("Most me a postive number")
-    print(x)
-
+while num != 67:
+    num = int(input("Enter a positive number… enter 67 to end the program: "))
+    if num < 0:
+        print("Must me a postive number")
+    tottal += num
     
-
-
+    
+    
+print(tottal-67)
 
 '''
 Write a program that asks the user to enter a positive number.  The program should check to see if the user entered a negative number and respond accordingly. The program should use a WHILE loop to keep asking the user to enter a number or enter 67 to stop (This will be the Sentinel that terminates your program). Your while loop should keep a running total (sum) of the numbers entered by the user (except if they enter 67). 67 shouldn’t be added.  
