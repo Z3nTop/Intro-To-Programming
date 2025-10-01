cb0352c02acbe69eba083b0665bcb48de69d4ce2
Zen <exeat@pronton.me>
2025-10-01 10:07:56 -0400

    Was shwon a better way to do this

--- quiz/toppenbergQuiz4.py
+++ quiz/toppenbergQuiz4.py
@@ -1,5 +1,6 @@
-number = 1
+number = int(input("etner a number form 1-8 "))
+
 while number >= 1 and number <= 8:
+    print("Mr. Todaro smells like hot dog water")
     number = int(input("etner a number form 1-8 "))
-    if number >= 1 and number <= 8:
-        print("Mr. Todaro smells like hot dog water")
+    
