dd83a59cfb75b0f9792e4bbf66af8fe9b8897b81
Zen <exeat@pronton.me>
2025-10-01 10:07:24 -0400

    Some last min work

--- Labs/Lab4/toppenbergLab4.py
+++ Labs/Lab4/toppenbergLab4.py
@@ -5,47 +5,30 @@ Solution:       toppenbergLab4
 Developer:      Zen Toppenberg
 Course:         Intro to Programming & Logic – CITC-1301-H91
 Creation Date:  9/28/25)
-Last Mod Date:  9/29/25)
+Last Mod Date:  10/1/25)
 E-mail Address: nxtoppenberg@senators.ws.edu
 -----------------------------------------------------------------------------
-Purpose - <general description of program's purpose>
+Purpose - to add all given numbers tht aren't 67
 -----------------------------------------------------------------------------
 Description of input:
 Numbers to be add together
 Description of output:
-Adds all of the given number
+Adds all of the given number added together
 -----------------------------------------------------------------------------
 '''
 #sets vars for the program
 num = 0
 tottal = 0
 
-
-#Takes the user input and adds it to it's self
+#Asks the user for numbers and will keep going until the user inputs 67
 while num != 67:
     num = int(input("Enter a positive number… enter 67 to end the program: "))
     if num < 0:
         print("Must me a postive number")
+#This part stops the negetice number form beinging added to the tottal
         num = 0
     tottal += num
     
 
-#Need to find a better way to do this
-#Also need to fix that non postive numbers being added
-print(tottal-67)
-
-'''
-Write a program that asks the user to enter a positive number.  The program should check to see if the user entered a negative number and respond accordingly. The program should use a WHILE loop to keep asking the user to enter a number or enter 67 to stop (This will be the Sentinel that terminates your program). Your while loop should keep a running total (sum) of the numbers entered by the user (except if they enter 67). 67 shouldn’t be added.  
-
-Sample Input
-Write a program that asks the user to enter a positive number.  The program should check to see if the user entered a negative number and respond accordingly. The program should use a WHILE loop to keep asking the user to enter a number or enter 67 to stop (This will be the Sentinel that terminates your program). Your while loop should keep a running total (sum) of the numbers entered by the user (except if they enter 67). 67 shouldn’t be added.  
-
-Sample Input
-Enter a positive number… enter 67 to end the program:  3
-Enter a positive number… enter 67 to end the program:  8 
-Enter a positive number… enter 67 to end the program:  67
-The Sum of the numbers entered is:  11:  3
-Enter a positive number… enter 67 to end the program:  8 
-Enter a positive number… enter 67 to end the program:  67
-The Sum of the numbers entered is:  11
-'''
+#Prints the sum of all numbers given munus 67
+print("The sum of the numbers is", tottal-67)
