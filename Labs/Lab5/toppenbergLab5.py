c93c552337ff56d01cca419b7d12849e0f4e3d4e
Zen <exeat@pronton.me>
2025-10-07 14:37:15 -0400

    Day 2 I guess

--- Labs/Lab5/toppenbergLab5.py
+++ Labs/Lab5/toppenbergLab5.py
@@ -5,7 +5,7 @@ Solution:       toppenbergLab
 Developer:      Zen Toppenberg
 Course:         Intro to Programming & Logic – CITC-1301-H91
 Creation Date:  10/6/25)
-Last Mod Date:  10/6/25)
+Last Mod Date:  10/7/25)
 E-mail Address: nxtoppenberg@senators.ws.edu
 -----------------------------------------------------------------------------
 Purpose - Converts human years to dog years useing funtions
@@ -32,24 +32,28 @@ THREE_PLUS_YEARS_MULTIPLIER = 5
 print("This program calculate's a dog's approximate age in \"dog years\" based on human years.\n")
 
 # Get human years from user
-humanYears = float(input("Dog's age in human years? "))
 
-if humanYears < 0:
-    print("\nHuman age must be a positive number.")
-else:
-    # Human age to dog age calculation
-    if humanYears <= 1:
-        dogYears = FIRST_YEAR_EQUIV * humanYears
-    elif humanYears <= 2:
-        dogYears = FIRST_YEAR_EQUIV + SECOND_YEAR_EQUIV * (humanYears - 1)
+def getDogYears():
+    humanYears = float(input("Dog's age in human years? "))
+
+    while humanYears < 0:
+        humanYears = float(input("\nHuman age must be a positive number. "))
+        
     else:
-        dogYears = FIRST_YEAR_EQUIV + SECOND_YEAR_EQUIV + THREE_PLUS_YEARS_MULTIPLIER * (humanYears - 2)
-    # end if
+        # Human age to dog age calculation
+        if humanYears <= 1:
+            dogYears = FIRST_YEAR_EQUIV * humanYears
+        elif humanYears <= 2:
+            dogYears = FIRST_YEAR_EQUIV + SECOND_YEAR_EQUIV * (humanYears - 1)
+        else:
+            dogYears = FIRST_YEAR_EQUIV + SECOND_YEAR_EQUIV + THREE_PLUS_YEARS_MULTIPLIER * (humanYears - 2)
+        # end if
 
-    print("\nA dog with a human age of", format(humanYears, ",.1f"), "years is", 
-        format(dogYears, ",.1f"), "in dog years.")
+        print("\nA dog with a human age of", format(humanYears, ",.1f"), "years is", 
+            format(dogYears, ",.1f"), "in dog years.")
 # end if
 
+getDogYears()
 
 
 '''
