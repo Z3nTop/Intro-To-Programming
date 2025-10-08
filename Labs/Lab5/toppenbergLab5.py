e1a608b52d36ced7b54b6b140397dd7ddfbd33bd
Zen <exeat@pronton.me>
2025-10-08 10:45:24 -0400

    Almost done just need to add for coms

--- Labs/Lab5/toppenbergLab5.py
+++ Labs/Lab5/toppenbergLab5.py
@@ -17,9 +17,7 @@ Description of output:
 -----------------------------------------------------------------------------
 '''
 
-
 # Lab3 - Human to Dog Years
-
 
 # Constants
 FIRST_YEAR_EQUIV = 15
@@ -32,36 +30,45 @@ THREE_PLUS_YEARS_MULTIPLIER = 5
 print("This program calculate's a dog's approximate age in \"dog years\" based on human years.\n")
 
 # Get human years from user
+def getDogYears(humanYears):
+    # Human age to dog age calculation
+    if humanYears <= 1:
+        dogYears = FIRST_YEAR_EQUIV * humanYears
+    elif humanYears <= 2:
+        dogYears = FIRST_YEAR_EQUIV + SECOND_YEAR_EQUIV * (humanYears - 1)
+    else:
+        dogYears = FIRST_YEAR_EQUIV + SECOND_YEAR_EQUIV + THREE_PLUS_YEARS_MULTIPLIER * (humanYears - 2)
+    return dogYears
 
-def getDogYears():
+
+def main():
+    #Asks the user for the Dogs age in human years
     humanYears = float(input("Dog's age in human years? "))
-
+    #Checks if the given input is a postive number
     while humanYears < 0:
         humanYears = float(input("\nHuman age must be a positive number. "))
-        
-    else:
-        # Human age to dog age calculation
-        if humanYears <= 1:
-            dogYears = FIRST_YEAR_EQUIV * humanYears
-        elif humanYears <= 2:
-            dogYears = FIRST_YEAR_EQUIV + SECOND_YEAR_EQUIV * (humanYears - 1)
-        else:
-            dogYears = FIRST_YEAR_EQUIV + SECOND_YEAR_EQUIV + THREE_PLUS_YEARS_MULTIPLIER * (humanYears - 2)
-        # end if
-
-        print("\nA dog with a human age of", format(humanYears, ",.1f"), "years is", 
-            format(dogYears, ",.1f"), "in dog years.")
+    #Converts humans to dogs years using the funtion
+    calculatedDogYears = getDogYears(humanYears)
+    #Outputs the converted number of years
+    print("\nA dog with a human age of", format(humanYears, ",.1f"), "years is", 
+        format(calculatedDogYears, ",.1f"), "in dog years.")
 # end if
 
-getDogYears()
+#Sets var for while loop
+loops = True
 
 
-'''
-Given task
-Start with the solution for Chapter 3 Project (or by copying/pasting the code below into an editor). Modify the "Human to Dog Years" program with the following enhancements:
-    • If the user enters a value less than zero for humanYears, use a loop to inform the user that "Human years must be a positive number" and ask the user to enter humanYears again (until the user enters an acceptable value).
-    • Move the logic used to determine the human years to dog years calculation into a function, called: getDogYears(). The getDogYears() function accepts one floating-point argument, called: humanYears. Based on the passed-in humanYears argument, the getDogYears() function calculates and returns the equivalent age in dog years as a floating-point value (i.e., the function should not return a string). 
-Note: Do not output anything to the screen from the getDogYears() function! Output to screen should only occur from the main() function.
-    • Use a loop to allow the user to calculate another human to dog years if they so choose.
-Human To Dog Years program source code
-'''
+#Starts a loop for the program to 
+while loops:
+    main()
+    testRepeat = True
+    while testRepeat:
+        ask = input("Do you want to convert again? y/n ")
+        if ask == "y" or ask =="Y":
+            loops = True
+            testRepeat = False
+        elif ask == "n" or ask == "N":
+            loops = False
+            testRepeat = False
+        else:
+            print("what? ")
