79c78eb319bb3cbc9aef3e8027e237d5599a25c8
ZenExeat <you@example.com>
2025-10-15 17:23:48 -0400

    Some more work but not ready for summtion

--- Labs/Lab6/toppenbergLab6.py
+++ Labs/Lab6/toppenbergLab6.py
@@ -5,49 +5,53 @@ Solution:       toppenbergLab
 Developer:      Zen Toppenberg
 Course:         Intro to Programming & Logic – CITC-1301-H91
 Creation Date:  10/1/25)
-Last Mod Date:  10/1/25)
+Last Mod Date:  10/15/25)
 E-mail Address: nxtoppenberg@senators.ws.edu
 -----------------------------------------------------------------------------
-Purpose - <general description of program's purpose>
+Purpose - generates a number from 1-20 and has user guess it
 -----------------------------------------------------------------------------
 Description of input:
-<short description of program's inputs>
+Guessed at what the number is
 Description of output:
-<short description of program's outputs>
+Tells the user if the guessed number is higher, lower or the curret number
 -----------------------------------------------------------------------------
 '''
-#inports random and sets random number for the guess
-import random
-answer = random.randint(1, 20)
-guesses = 0
+def main(guesses):
+    #inports random and sets random number for the guess
+    import random
+    answer = random.randint(1, 20)
+    guesses = 0
 
-print("Your lifelong dream is to be on the Jedi Council")
-print("Your opportunity comes and you get an interview with Yoda.")
-print("He asks you to guess a number between 1 and 20.")
+    print("Your lifelong dream is to be on the Jedi Council")
+    print("Your opportunity comes and you get an interview with Yoda.")
+    print("He asks you to guess a number between 1 and 20.")
 
 
-#Runs a check 
-isRunning = True
-while isRunning:
-    #REWMOVE BEFORE SUMMINTING 
-    print(answer)
-    guess = int(input("What is your guess? "))
-    if guess == answer:
-        isRunning = False
-        guesses += 1
-    elif guess > answer:
-        print("Your guess is too high")
-        guesses += 1
-    else:
-        print("Your guess is too low")
-        guesses += 1
-        
+    #Runs a check 
+    isRunning = True
+    while isRunning:
+        #REWMOVE BEFORE SUMMINTING 
+        print(answer)
+        guess = int(input("What is your guess? "))
+        if guess == answer:
+            isRunning = False
+            guesses += 1
+        elif guess > answer:
+            print("Your guess is too high")
+            guesses += 1
+        else:
+            print("Your guess is too low")
+            guesses += 1
+    return guesses
+
+
+main(guesses)
+
 #Runs the different messages based on the number of guess is
 if guesses <= 3:
     print("Congratulations, been accepted to the Jedi Council you have")
 else:
     print("Ready for the Jedi Council you are not. Weak with the force you are!")
-
 
 
 '''
