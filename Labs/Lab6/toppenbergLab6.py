50642d8584333df954688b90d3f41d939944e061
Zen <exeat@pronton.me>
2025-10-16 13:48:40 -0400

    Should be ready to summber after I get email form Pro

--- Labs/Lab6/toppenbergLab6.py
+++ Labs/Lab6/toppenbergLab6.py
@@ -16,52 +16,40 @@ Description of output:
 Tells the user if the guessed number is higher, lower or the curret number
 -----------------------------------------------------------------------------
 '''
-def main(guesses):
+def main():
     #inports random and sets random number for the guess
     import random
     answer = random.randint(1, 20)
     guesses = 0
 
+
+    #Displays the starting text
     print("Your lifelong dream is to be on the Jedi Council")
     print("Your opportunity comes and you get an interview with Yoda.")
     print("He asks you to guess a number between 1 and 20.")
 
 
-    #Runs a check 
+    #Runs a check to see if the guessed number is the numbere that was genatraed 
     isRunning = True
     while isRunning:
         #REWMOVE BEFORE SUMMINTING 
         print(answer)
         guess = int(input("What is your guess? "))
+        guesses += 1
         if guess == answer:
             isRunning = False
-            guesses += 1
         elif guess > answer:
             print("Your guess is too high")
-            guesses += 1
         else:
             print("Your guess is too low")
-            guesses += 1
     return guesses
 
 
-main(guesses)
+guesses = main()
+
 
 #Runs the different messages based on the number of guess is
 if guesses <= 3:
     print("Congratulations, been accepted to the Jedi Council you have")
 else:
     print("Ready for the Jedi Council you are not. Weak with the force you are!")
-
-
-'''
-Your lifelong dream is to be on the Jedi Council.  
-Your opportunity comes and you get an interview with Yoda. 
-He asks you to guess a number between 1 and 20.  
-You need to write a program that creates a random number from 1 to 20 and then asks a guess from the user.  
-If the guess is higher than the random number then you need to display “Too High, Guess Again!”  
-If the guess is lower than the random number you need to display “Too Low, Guess Again!”  
-The program should keep asking until the number is guessed. Once the number is guessed, you need to display 
-“Congratulations, been accepted to the Jedi Council you have ” only if the user number of guesses are 3 or less.
-If the number of guesses is more than 3 then print “Ready for the Jedi Council you are not. Weak with the force you are!”
-'''
