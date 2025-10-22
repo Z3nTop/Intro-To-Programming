7d2e717214733370342547b2855e465897d30bb7
Zen <exeat@pronton.me>
2025-10-22 10:49:50 -0400

    Should be done

--- Labs/Lab7/toppenberLab7.py
+++ Labs/Lab7/toppenberLab7.py
@@ -5,7 +5,7 @@ Solution:       toppenbergLab
 Developer:      Zen Toppenberg
 Course:         Intro to Programming & Logic – CITC-1301-H91
 Creation Date:  10/20/25)
-Last Mod Date:  10/20/25)
+Last Mod Date:  10/22/25)
 E-mail Address: nxtoppenberg@senators.ws.edu
 -----------------------------------------------------------------------------
 Purpose - Gives random stats for a character
@@ -13,7 +13,7 @@ Purpose - Gives random stats for a character
 Description of input:
 NONE
 Description of output:
-The sheet with random stats
+The sheet with random stats to character.txt
 -----------------------------------------------------------------------------
 '''
 
@@ -29,34 +29,20 @@ def main():
     dexterity = random.randint(1, 18)
     charisma = random.randint(1, 18)
     
-
+    #opens the file for writing
     outfile = open('character.txt', 'w')
 
-    
+    #Outputs the random stats to the file
     outfile.write("Strength   "  + str(strength)+ "\n")
     outfile.write("constitution   "  + str(constitution)+ "\n")
     outfile.write("wisdom   "  + str(wisdom)+ "\n")
     outfile.write("dexterity   "  + str(strength)+ "\n")
     outfile.write("charisma   "  + str(charisma)+ "\n")
 
-
+    #closes the file
+    file.close()
 
 
 #Calls main
 if __name__ == "__main__":
     main()
-
-
-""""
-Write a program that creates a character.txt file and writes to it (On its own line) 
-the word Strength, Constitution,  Wisdom, Intelligence, Dexterity and Charisma. 
-After each word you need place a RANDOM number from 1 to 18 for each attribute. 
-So the file should have something like this:
-
-Strength 12
-Constitution 13
-Wisdom 9
-Dexterity 9
-Charisma 18
-REMEMBER each time I run your program it should create a new character.txt file with difference random numbers
-"""
