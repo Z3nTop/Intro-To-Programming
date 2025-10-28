db820f49833210033caa8cca62716ae22d352fb1
Zen <exeat@pronton.me>
2025-10-28 08:37:35 -0400

    work I guess

--- Tests/toppenbergTest2-b.py
+++ Tests/toppenbergTest2-b.py
@@ -4,8 +4,8 @@ Solution:       toppenbergLab
 -----------------------------------------------------------------------------
 Developer:      Zen Toppenberg
 Course:         Intro to Programming & Logic – CITC-1301-H91
-Creation Date:  10/1/25)
-Last Mod Date:  10/1/25)
+Creation Date:  10/25/25)
+Last Mod Date:  10/28/25)
 E-mail Address: nxtoppenberg@senators.ws.edu
 -----------------------------------------------------------------------------
 Purpose - <general description of program's purpose>
@@ -28,16 +28,20 @@ def main():
     #Asks the user for the price of the itmes
     print("Put in the price of your itmes when you are done type -1")
     while looping:
+
         #Starts a loop adding the price to total and adds one to the item number
         price = float(input("Item"f'{item_num}'": "))
         if price == -1:
             looping = False
         total = total + price
         item_num += 1
+
     #Bandaid fix for the -1 beibg added to the total
     total += 1
+
     #Printes the price of all the items witout the round up down to 2 desmess
     print("Debit/Credit Price: "f'{total:.0f}' "")
+    print(round(total, 2))
 
 #Calls main
 if __name__ == "__main__":
